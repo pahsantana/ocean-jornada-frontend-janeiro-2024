904f9cb225eb13c90662cfaf2b0bf9a61f2c3fa6
Paloma Santana <62676609+pahsantana@users.noreply.github.com>
2024-01-30 14:39:09 +0000

    Implementação da lista de cards

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,20 @@ function App() {
     name: 'Summer Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
   }
+  const item4 = {
+    name: 'Beth Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+  }
+
+  const itens = [item1,item2, item3, item4]
+
   return (
     <>
       <div className="cards">
-        <Card item={item1}/>
+        {/* <Card item={item1}/>
         <Card item={item2}/>
-        <Card item={item3}/>
+        <Card item={item3}/> */} 
+        {itens.map(item => <Card item={item}/>)}
       </div>
     </>
   )
